a29178a13c620b993c221970bd08064ba7b003cd
Miguel Lessa <miguellessa3@gmail.com>
2025-04-03 18:27:35 -0300

    criação da classe Scanner

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new scanner(System.in).useLocale(Locale.US);
+        
+
+
+
+    }
+    
+    }
+
+
